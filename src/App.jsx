b8126d751d7e599a9edce4ed3230be9c0ded99c7
DeeSzubert <pietryd@yahoo.com>
2024-07-01 20:37:02 +0100

    check deploy

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <h1 style="colour: red">hello world</h1>
+        <h1>hello world</h1>
         <h1>Vite + React</h1>
       </div>
 
